5f1100f766e1139b1230ae72c2b3e1ecfe5cc0c5
Mateus Back <mateusfback14@gmail.com>
2023-10-21 17:02:19 -0300

    Calc. de aréas de triangulos utilizando objetos.

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -4,16 +4,33 @@ using System.Globalization;
 namespace Curso {
     internal class Program {
         public static void Main(string[] args) {
-                Console.Write("Digite um número: ");
-                double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
 
-                while(x >= 0.0) {
-                    double raiz = Math.Sqrt(x);
-                    Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
-                    Console.Write("Digite outro número: ");
-                    x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            } else {
+                Console.WriteLine("Maior área: Y");
             }
-            Console.WriteLine("Número Negativo!");
         }
     }
 }
