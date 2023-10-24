b53286c8552225ef9823419f586edb060153cd28
mateusback <mateusfback14@gmail.com>
2023-10-24 12:37:13 -0300

    Properties e encapsulamento.

--- Curso/Produto.cs
+++ Curso/Produto.cs
@@ -1,38 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Curso {
     internal class Produto {
-        public String Nome;
-        public double Preco;
-        public int Quantidade;
+        public String _nome;
+        public double _preco;
+        public int _quantidade;
 
+        //Properties
+        public String Nome{
+            get { return _nome; }
+            set { _nome = value; }
+        }
 
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade = Quantidade + quantidade;
+            _quantidade = _quantidade + quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade = Quantidade - quantidade;
+            _quantidade = _quantidade - quantidade;
         }
 
         public override string ToString() {
-            return Nome
+            return _nome
                 + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
 
         }
+        /*
+        public String GetNome(){
+            return _nome;
+        }
+        public void SetNome(String nome){
+            _nome = nome;
+        }*/
     }
 }
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -4,25 +4,10 @@ using System.Globalization;
 namespace Curso {
     internal class Program {
         public static void Main(string[] args) {
-            Console.Write("Nome: ");
-            String nome = Console.ReadLine();
-            Console.Write("Salário bruto: ");
-            double salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Imposto: ");
-            double imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           Produto produto = new Produto();
 
+            produto.Nome = "TV";
 
-            Funcionario funcionario = new Funcionario(nome, salarioBruto, imposto);
-
-            Console.WriteLine("Funcionario: " + funcionario.ToString());
-
-
-            Console.Write("Digite a porcentagem para aumentar o salário: ");
-            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            funcionario.AumentoSalario(porcentagem);
-
-            Console.WriteLine("Dados Ataulizados: " + funcionario.ToString());
         }
     }
 }
